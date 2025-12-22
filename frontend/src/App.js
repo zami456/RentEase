6dd3d5eb0222e439ff47e4b94661bd40c8ee7480
zami456 <abdullah.al.zami@g.bracu.ac.bd>
2025-12-22 08:10:35 +0600

    feat: setup frontend structure with initial configuration files

---
+++ frontend/src/App.js
@@ -0,0 +1,7 @@
+import { useEffect, useState } from "react";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import API_BASE_URL from "./config/api";
+
+// importing pages and components
+import Home from "./pages/home";
+
